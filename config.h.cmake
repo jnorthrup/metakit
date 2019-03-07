1f69a0b3bc7386888e9e2dbcb1b13dab44c1e2a7
Sam Bromley <sam@rov3d.com>
2019-03-07 17:47:21 -0330

    Initial support to CMake

---
+++ config.h.cmake
@@ -0,0 +1,15 @@
+/* Name of package */
+#cmakedefine PACKAGE @PACKAGE@
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT @PACKAGE_BUGREPORT@
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME @PACKAGE_NAME@
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING @PACKAGE_STRING@
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME @PACKAGE_TARNAME@
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION @PACKAGE_VERSION@
+/* Version number of package */
+#cmakedefine VERSION @VERSION@
+#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
---
+++ tcl/pkgIndex.tcl
@@ -0,0 +1,3 @@
+package ifneeded Mk4tcl 2.4.9.7 \
+    [list load [file join $dir mk4tcl.so] Mk4tcl]
+
